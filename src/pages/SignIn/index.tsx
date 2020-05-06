d7c5e24ea23b4231049f340fa920b1d11944f9ed
Cesar Denis <cesar@cesardenis.com>
2020-05-05 23:00:25 -0300

    feat: add login page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import imgLogo from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={imgLogo} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu login</h1>
+
+        <input type="email" placeholder="E-mail" />
+
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="forgot">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
