d7009c69a8093333ad1986c8eaf8fb8b7c1fbf2b
Cesar Denis <cesar@cesardenis.com>
2020-08-07 09:06:01 -0300

    feat: add toast layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import { AuthProvider } from './contexts/AuthContext';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
+
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
